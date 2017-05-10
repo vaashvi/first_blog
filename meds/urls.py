2ce1b00becce1a224c8bf2a22a870354c104bdf6
vaashvi <aashvi1996@gmail.com>
2017-05-10 17:38:48 +0530

    Update urls.py

--- meds/urls.py
+++ meds/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import include,url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from promeds.views import homepage
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
