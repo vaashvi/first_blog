695dc53e4f35e7f4b79090bcd676f6a4b22dda20
vaashvi <aashvi1996@gmail.com>
2017-05-10 20:32:30 +0530

    Update urls.py

--- meds/urls.py
+++ meds/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^home/',include('home.urls',namespace='home')),
+    url(r'^home/',include('promeds.urls')),
     url(r'^promeds/',include('promeds.urls')),
 #url(r'^resetpassword/passwordsent/$', 'django.contrib.auth.views.password_reset_done',name='password_reset_done'),
 #url(r'^reset/password_reset/$', 'django.contrib.auth.views.password_reset', name='reset_password_reset1'),
