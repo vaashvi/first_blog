2d555988bcf4000e613b098026eff4c2c00a92e8
vaashvi <aashvi1996@gmail.com>
2017-05-10 19:04:29 +0530

    Update views.py

--- promeds/views.py
+++ promeds/views.py
@@ -1,4 +1,3 @@
-import pytz as pytz
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render,redirect,render_to_response
 from django.contrib.auth import authenticate,logout
